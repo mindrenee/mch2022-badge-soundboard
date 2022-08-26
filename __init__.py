ac656ca2bc1736d9953317102b015f64bae90a7b
mindrenee <r.witsenburg@atlascollege.nl>
2022-08-26 22:58:23 +0200

    New sounds + Pagination

--- __init__.py
+++ __init__.py
@@ -5,11 +5,15 @@ import buttons
 import display
 import mch22
 
-SOUND_BASE_DIR="/apps/python/soundboard/sounds"
+SOUND_BASE_DIR="/sd/apps/python/soundboard/sounds"
 
 sounds = os.listdir(SOUND_BASE_DIR)
 n_sounds = len(os.listdir(SOUND_BASE_DIR))
 menuItem = 0
+page = 0
+sounditem = 0
+soundlists = [sounds[i:i + 13] for i in range(0, len(sounds), 13)]
+pages = len(soundlists)
 
 def play_random_sound():
     rand = random.randint(0, n_sounds - 1)
@@ -18,7 +22,8 @@ def play_random_sound():
     channel_id = audio.play(sound, volume=150)
 
 def play_selected_sound():
-    sound = SOUND_BASE_DIR + "/" +  sounds[menuItem]
+    print(sounds[sounditem])
+    sound = SOUND_BASE_DIR + "/" +  sounds[sounditem]
     channel_id = audio.play(sound, volume=150)
 
 def on_action_btn(pressed):
@@ -26,36 +31,77 @@ def on_action_btn(pressed):
         #play_random_sound()
         play_selected_sound()
 
-def renderMenu(nextMenuItem):
+def renderMenu(nextMenuItem, nextpage):
+    global sounditem
     global menuItem
+    global page
     y = 5
-    print(display.size())
-    # TODO: Scroll if display height is reached (y = 240px)
+    # Background (white)
     display.drawFill(0xFFFFFF)
-    for i in range(len(sounds)):
-        if i == nextMenuItem:
-            display.drawText(10, y, sounds[i], 0xFF0000)
+
+    display.drawText(135, 220, "<", 0x000000)
+    x = 150
+    for j in range(pages):
+        if j == nextpage:
+            display.drawText(x, 220, str(j+1), 0xFF0000)
         else:
-            display.drawText(10, y, sounds[i], 0x000000)
+            display.drawText(x, 220, str(j+1), 0x000000)
+        x = x + 15
+    display.drawText(x, 220, ">", 0x000000)
+    page = nextpage
+
+    for i in range(len(soundlists[page])):
+        if i == nextMenuItem:
+            display.drawText(10, y, soundlists[page][i], 0xFF0000)
+            # Netter maken voor als we meer dan 2 pagina's hebben
+            if page != 0:
+                sounditem = i + 1 + len(soundlists[page]) + 1
+                print(sounds[sounditem])
+            else:
+                sounditem = nextMenuItem
+                print(sounds[sounditem])
+        else:
+            display.drawText(10, y, soundlists[page][i], 0x000000)
         y = y + 15
     display.flush()
     menuItem = nextMenuItem
-
+  
 def on_action_btn_up(pressed):
+    # TODO max 13 sounds per page
     if pressed:
         if menuItem == 0:
-            newMenuItem = n_sounds - 1
+            newMenuItem = len(soundlists[page]) - 1
         else:
             newMenuItem = menuItem - 1
-        renderMenu(newMenuItem)
+        renderMenu(newMenuItem, page)
 
 def on_action_btn_down(pressed):
+    # TODO max 13 sounds per page
     if pressed:
-        if menuItem == n_sounds:
+        if menuItem == len(soundlists[page]):
             newMenuItem = 0
         else:
             newMenuItem = menuItem + 1
-        renderMenu(newMenuItem)
+        renderMenu(newMenuItem, page)
+
+def on_action_btn_left(pressed):
+    if pressed:
+        newMenuItem = 0
+        if page == 0:
+            print("Left")
+            newpage = pages - 1
+        else:
+            newpage = page - 1
+        renderMenu(newMenuItem, newpage)
+
+def on_action_btn_right(pressed):
+    if pressed:
+        newMenuItem = 0
+        if page == pages:
+            newpage = 0
+        else:
+            newpage = page + 1
+        renderMenu(newMenuItem, newpage)
 
 def reboot(pressed):
   if pressed:
@@ -64,9 +110,11 @@ def reboot(pressed):
 
 buttons.attach(buttons.BTN_HOME,reboot)
 
-renderMenu(0)
+renderMenu(0, 0)
 buttons.attach(buttons.BTN_A, on_action_btn)
 buttons.attach(buttons.BTN_UP, on_action_btn_up)
 buttons.attach(buttons.BTN_DOWN, on_action_btn_down)
+buttons.attach(buttons.BTN_LEFT, on_action_btn_left)
+buttons.attach(buttons.BTN_RIGHT, on_action_btn_right)
 while True:
     continue
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 python3 ../mch2022-tools-master/webusb_fat_push.py __init__.py /sdcard/apps/python/soundboard/__init__.py
-python3 ../mch2022-tools-master/webusb_fat_push.py icon.png /sdcard/apps/python/soundboard/icon.png
+#python3 ../mch2022-tools-master/webusb_fat_push.py icon.png /sdcard/apps/python/soundboard/icon.png
 #for s in `ls sounds`; do
 #	echo $s
 #	python3 ../mch2022-tools-master/webusb_fat_push.py sounds/$s /sdcard/apps/python/soundboard/sounds/$s
